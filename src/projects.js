0c4573e7d0cd0021ebbdf8178d7fd9ee31df6613
Mohamed <Mohamed.m365@outlook.com>
2023-08-23 19:12:08 +0100

    worked on project screen

--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import './projects.css'
 
 function Projects() {
   return (
     <div className='Projects-section'>
-      {/* Your projects content here */}
+      <header className='projects-header'>
+      <h1>Projects</h1>
+      <p>Explore</p>
+      </header>
+      <div className='Projects-grid'>
+        <div className='Project-item'>
+          <img src='project1.jpg' alt='Project 1' />
+          <h2>Project Title 1</h2>
+          <div className='Project-links'>
+            <a href='#'>Live</a>
+            <a href='#'>GitHub</a>
+          </div>
+        </div>
+        <div className='Project-item'>
+          <img src='project2.jpg' alt='Project 2' />
+          <h2>Project Title 2</h2>
+          <div className='Project-links'>
+            <a href='#'>Live</a>
+            <a href='#'>GitHub</a>
+          </div>
+        </div>
+        <div className='Project-item'>
+          <img src='project3.jpg' alt='Project 3' />
+          <h2>Project Title 3</h2>
+          <div className='Project-links'>
+            <a href='#'>Live</a>
+            <a href='#'>GitHub</a>
+          </div>
+        </div>
+        <div className='Project-item'>
+          <img src='project4.jpg' alt='Project 4' />
+          <h2>Project Title 4</h2>
+          <div className='Project-links'>
+            <a href='#'>Live</a>
+            <a href='#'>GitHub</a>
+          </div>
+        </div>
+        <div className='Project-item'>
+          <img src='project5.jpg' alt='Project 5' />
+          <h2>Project Title 5</h2>
+          <div className='Project-links'>
+            <a href='#'>Live</a>
+            <a href='#'>GitHub</a>
+          </div>
+        </div>
+        <div className='Project-item'>
+          <img src='project6.jpg' alt='Project 6' />
+          <h2>Project Title 6</h2>
+          <div className='Project-links'>
+            <a href='#'>Live</a>
+            <a href='#'>GitHub</a>
+          </div>
+        </div>
+        </div>
+
     </div>
   );
 }
