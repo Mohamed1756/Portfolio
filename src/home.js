b8ed385b3c0b3c87728399e6a9b4c0fe0ea3427b
Mohamed <Mohamed.m365@outlook.com>
2023-08-29 15:57:15 +0100

    worked on home and project screens

--- src/home.js
+++ src/home.js
@@ -10,24 +10,55 @@ function Home() {
        <p>SOFTWARE ENGINEER | NYC</p>
     </header>
     <div className="Skills-certificates-section">
-       <div class="Skills-section">
-          <h2>SKILLS</h2>
-          <div class="Skills-table">
-             <div class="Skills-category">
-                <h4>Languages: JavaScript | Python | Java | TypeScript</h4>
-             </div>
-             <div class="Skills-category">
-                <h4>Databases: MongoDB | SQL | Firebase | Redis</h4>
-             </div>
-             <div class="Skills-category">
-                <h4>Front-End: React | Next.js | Bootstrap | HTML | CSS </h4>
-             </div>
-             <div class="Skills-category">
-                <h4>Backend: Node.js | Rest API | PostMan | SpringBoot </h4>
-             </div>
-         </div>
-       </div>
-    
+    <div class="Skills-section">
+  <h2>SKILLS</h2>
+  <div class="skills-list">
+  <div class="skill-row">
+    <div class="skill languages">
+      <h4>Languages</h4>
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript" class="skill-icon"/>
+            
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" alt="Python" class="skill-icon"/>
+      
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" alt="Java" class="skill-icon"/>
+     
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="TypeScript" class="skill-icon"/>
+    </div>
+    <div class="skill databases">
+      <h4>Database</h4>
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="MongoDB" class="skill-icon"/>
+      
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="SQL" class="skill-icon"/>
+      
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg" alt="Firebase" class="skill-icon"/>
+      
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original-wordmark.svg" alt="Redis" class="skill-icon"/>
+    </div>
+    </div>
+    <div class="skill-row">
+    <div class="skill frontend">
+      <h4>Front-end</h4>
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="React" class="skill-icon"/>
+     
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg" alt="Next.js" class="skill-icon"/> 
+      
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" alt="HTML" class="skill-icon"/>
+      
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" alt="CSS" class="skill-icon"/>
+    </div>
+    <div class="skill backend">
+      <h4>Backend</h4>
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"  alt="Node.js" class="skill-icon"/>
+
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg"  alt="Express.js" class="skill-icon" />
+          
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original-wordmark.svg" alt="Postman" class="skill-icon"/>
+      
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg" alt="Spring Boot" class="skill-icon"/>
+    </div>
+    </div>
+  </div>
+</div>
     <div className='Certificates-section'>
        <h2>Certificates</h2>
        <div className="Certificate-list">
@@ -36,7 +67,7 @@ function Home() {
              <a href="link_to_certificate2" target="_blank" rel="noopener noreferrer">
              <img src="azure.svg" alt="azure img" />
              </a> 
-             (Microsoft Azure AI Fundamentals: <a href="https://learn.microsoft.com/en-gb/certifications/exams/ai-900/?wt.mc_id=learningredirect_certs-web-wwl" target="_blank" rel="noopener noreferrer">AI-900</a>)
+             (Microsoft Azure AI Fundamentals: <a href="https://learn.microsoft.com/en-gb/certifications/exams/ai-900/?wt.mc_id=learningredirect_certs-web-wwl" target="_blank" rel="noopener noreferrer"> AI-900</a>)
           </div>
           <div className="Certificate-item">
              <a href="link_to_certificate3" target="_blank" rel="noopener noreferrer">
@@ -60,21 +91,21 @@ function Home() {
          <div class="Contact-columns">
             <div class="Contact-col">
                <span class="Contact-icon">
-               <img src="/envelope-solid.svg" alt="Email" />
+               <a href="mailto:Mohamed.m365@outlook.com"><img src="/envelope-solid.svg" alt="Email" /></a>
                </span>
-               <a href="mailto:Mohamed.m365@outlook.com">Email</a>
+              
             </div>
             <div class="Contact-col">
                <span class="Contact-icon">
-               <img src="square-github.svg" alt="GitHub" />
+               <a href="https://github.com/Mohamed1756"><img src="square-github.svg" alt="GitHub" /></a>
                </span>
-               <a href="https://github.com/Mohamed1756">GitHub</a>
+              
             </div>
             <div class="Contact-col">
                <span class="Contact-icon">
-               <img src="Linkedin.svg" alt="LinkedIn" />
+               <a href="https://www.linkedin.com/in/mohamed-m-87a213214/"><img src="Linkedin.svg" alt="LinkedIn" /></a>
                </span>
-               <a href="https://www.linkedin.com/in/mohamed-m-87a213214/">LinkedIn</a>
+              
             </div>
          </div>
       </div>
--- src/projects.js
+++ src/projects.js
@@ -14,47 +14,47 @@ function Projects() {
           <img src='test2.png' alt='Project 1' />
           <div className='Project-links'>
             <a href='#'>Live</a>
-            <a href='#'>GitHub</a>
+            <a href='https://github.com/Mohamed1756/summary-app'>GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
-          <h2>Project Title 2</h2>
+          <h2>Football Stats App</h2>
           <img src='test5.png' alt='Project 2' />
           <div className='Project-links'>
             <a href='#'>Live</a>
-            <a href='#'>GitHub</a>
+            <a href='https://github.com/Mohamed1756/Football-Data-Visualization'>GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
           <h2>Binance Liquidations Data</h2>
           <img src='test4.png' alt='Project 3' />
           <div className='Project-links'>
-            <a href='#'>Live</a>
-            <a href='#'>GitHub</a>
+            <a href='http://binance-liquidations.netlify.app/'>Live</a>
+            <a href='https://github.com/Mohamed1756/Binance-Liquidation-Data'>GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
-          <h2>Project Title 4</h2>
+          <h2>Email Spam Detector</h2>
           <img src='email.png' alt='Project 4' />
           <div className='Project-links'>
-            <a href='#'>Live</a>
-            <a href='#'>GitHub</a>
+            <a href='#' class='Link-tbc' data-tooltip='tbc'>Live</a>
+            <a href='https://github.com/Mohamed1756/spamDetection'>GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
-          <h2>Project Title 5</h2>
+          <h2>Social Kit </h2>
           <img src='test8.png' alt='Project 5' />
           <div className='Project-links'>
-            <a href='#'>Live</a>
-            <a href='#'>GitHub</a>
+            <a href='#' class='Link-tbc' data-tooltip='tbc'>Live</a>
+            <a href='https://github.com/Mohamed1756/SocialKit'>GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
-          <h2>Project Title 6</h2>
-          <img src='test.png' alt='Project 6' />
+          <h2>BetterLife (iOS) </h2>
+          <img src='betterLife.png' alt='Project 6' />
           <div className='Project-links'>
-            <a href='#'>Live</a>
-            <a href='#'>GitHub</a>
+            <a href='#' class='Link-tbc' data-tooltip='tbc'>Live</a>
+            <a href='https://github.com/Mohamed1756/BetterLife'>GitHub</a>
           </div>
         </div>
         </div>
