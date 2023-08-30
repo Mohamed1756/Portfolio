11abb9eec8111f5309640bef5ad2513226363ad6
Mohamed <Mohamed.m365@outlook.com>
2023-08-30 15:33:40 +0100

    finished landing page

--- src/home.js
+++ src/home.js
@@ -3,12 +3,31 @@ import { Link } from 'react-router-dom';
 import './App.css'
 
 function Home() {
+   
   return (
     <div className="Home">
     <header className="Home-header">
-       <h1>NAME</h1>
-       <p>SOFTWARE ENGINEER | NYC</p>
+       <h1>Mohamed A.M </h1>
+       <p>SOFTWARE ENGINEER | LONDON</p>
     </header>
+    <div style={{ textAlign: "center" }}>
+  <p>
+    Hey there! I'm a graduate in <strong>Computer Science</strong> with a keen
+    interest in creating seamless user experiences through{" "}
+    <strong>frontend</strong> and <strong>backend technologies</strong>.<br />
+    <br />
+    I'm captivated by emerging tech trends, from <strong>AI</strong> and{" "}
+    <strong>machine learning</strong> to exploring the potential of{" "}
+    <strong>web3</strong> and <strong>blockchain</strong>.
+  </p>
+  <p style={{ textAlign: "center", marginTop: "20px" }}>
+    My goal is to continue learning, and contribute to the{" "}
+    <strong>ever-evolving tech landscape</strong>.
+  </p>
+</div>
+
+
+
     <div className="Skills-certificates-section">
     <div class="Skills-section">
   <h2>SKILLS</h2>
@@ -25,7 +44,7 @@ function Home() {
       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="TypeScript" class="skill-icon"/>
     </div>
     <div class="skill databases">
-      <h4>Database</h4>
+      <h4>Databases</h4>
       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="MongoDB" class="skill-icon"/>
       
       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="SQL" class="skill-icon"/>
@@ -37,7 +56,7 @@ function Home() {
     </div>
     <div class="skill-row">
     <div class="skill frontend">
-      <h4>Front-end</h4>
+      <h4>Front-End</h4>
       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="React" class="skill-icon"/>
      
       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg" alt="Next.js" class="skill-icon"/> 
@@ -64,26 +83,26 @@ function Home() {
        <div className="Certificate-list">
           
           <div className="Certificate-item">
-             <a href="link_to_certificate2" target="_blank" rel="noopener noreferrer">
+          
              <img src="azure.svg" alt="azure img" />
-             </a> 
-             (Microsoft Azure AI Fundamentals: <a href="https://learn.microsoft.com/en-gb/certifications/exams/ai-900/?wt.mc_id=learningredirect_certs-web-wwl" target="_blank" rel="noopener noreferrer"> AI-900</a>)
+          
+             (Microsoft Azure AI Fundamentals:&nbsp;<a href="https://learn.microsoft.com/en-gb/certifications/exams/ai-900/?wt.mc_id=learningredirect_certs-web-wwl" target="_blank" rel="noopener noreferrer"> AI-900</a>)
           </div>
           <div className="Certificate-item">
-             <a href="link_to_certificate3" target="_blank" rel="noopener noreferrer">
+
              <img src="azure.svg" alt="azure img" />
-             </a> 
-             (Microsoft Azure Data Fundamentals: <a href="https://learn.microsoft.com/en-gb/certifications/exams/dp-900/?wt.mc_id=learningredirect_certs-web-wwl" target="_blank" rel="noopener noreferrer">DP-900</a>)
+          
+             (Microsoft Azure Data Fundamentals:&nbsp; <a href="https://learn.microsoft.com/en-gb/certifications/exams/dp-900/?wt.mc_id=learningredirect_certs-web-wwl" target="_blank" rel="noopener noreferrer">DP-900</a>)
           </div>
        </div>
     </div>
     </div>
     <div className='Projects-section'>
-       <span className="Contact-icon">
-       <img src="arrow-right.svg" alt="arrow-img" />
-       <Link to="/projects">Explore Projects</Link>
-       </span>
-    </div>
+  <span className="Contact-icon">
+    <img src="arrow-right.svg" alt="arrow-img" />
+    <Link to="/projects" className="Explore-button">Explore Projects</Link>
+  </span>
+</div>
 
    <div className='CV-Contact-section'>
       <div class="Contact-section">
@@ -97,16 +116,17 @@ function Home() {
             </div>
             <div class="Contact-col">
                <span class="Contact-icon">
-               <a href="https://github.com/Mohamed1756"><img src="square-github.svg" alt="GitHub" /></a>
+               <a href="https://www.linkedin.com/in/mohamed-m-87a213214/" target="_blank" rel="noopener noreferrer"><img src="Linkedin.svg" alt="LinkedIn" /></a>
                </span>
               
             </div>
             <div class="Contact-col">
                <span class="Contact-icon">
-               <a href="https://www.linkedin.com/in/mohamed-m-87a213214/"><img src="Linkedin.svg" alt="LinkedIn" /></a>
+               <a href="https://github.com/Mohamed1756" target="_blank" rel="noopener noreferrer"><img src="square-github.svg" alt="GitHub" /></a>
                </span>
               
             </div>
+           
          </div>
       </div>
 
--- src/projects.js
+++ src/projects.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './projects.css'
 
 function Projects() {
   return (
     <div className='Projects-section'>
+  
       <header className='projects-header'>
       <h1>Projects</h1>
       <p>Explore</p>
       </header>
+      <Link to="/" className="Back-button">
+        <i className="fas fa-arrow-left"></i> Back to Home
+      </Link>
       <div className='Projects-grid'>
       <div className='Project-item'>
           <h2>SummarEase</h2>
           <img src='test2.png' alt='Project 1' />
           <div className='Project-links'>
             <a href='#'>Live</a>
-            <a href='https://github.com/Mohamed1756/summary-app'>GitHub</a>
+            <a href='https://github.com/Mohamed1756/summary-app' target="_blank" rel="noopener noreferrer">GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
@@ -22,15 +27,15 @@ function Projects() {
           <img src='test5.png' alt='Project 2' />
           <div className='Project-links'>
             <a href='#'>Live</a>
-            <a href='https://github.com/Mohamed1756/Football-Data-Visualization'>GitHub</a>
+            <a href='https://github.com/Mohamed1756/Football-Data-Visualization' target="_blank" rel="noopener noreferrer">GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
           <h2>Binance Liquidations Data</h2>
           <img src='test4.png' alt='Project 3' />
           <div className='Project-links'>
-            <a href='http://binance-liquidations.netlify.app/'>Live</a>
-            <a href='https://github.com/Mohamed1756/Binance-Liquidation-Data'>GitHub</a>
+            <a href='http://binance-liquidations.netlify.app/' target="_blank" rel="noopener noreferrer">Live</a>
+            <a href='https://github.com/Mohamed1756/Binance-Liquidation-Data' target="_blank" rel="noopener noreferrer">GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
@@ -38,7 +43,7 @@ function Projects() {
           <img src='email.png' alt='Project 4' />
           <div className='Project-links'>
             <a href='#' class='Link-tbc' data-tooltip='tbc'>Live</a>
-            <a href='https://github.com/Mohamed1756/spamDetection'>GitHub</a>
+            <a href='https://github.com/Mohamed1756/spamDetection' target="_blank" rel="noopener noreferrer">GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
@@ -46,7 +51,7 @@ function Projects() {
           <img src='test8.png' alt='Project 5' />
           <div className='Project-links'>
             <a href='#' class='Link-tbc' data-tooltip='tbc'>Live</a>
-            <a href='https://github.com/Mohamed1756/SocialKit'>GitHub</a>
+            <a href='https://github.com/Mohamed1756/SocialKit' target="_blank" rel="noopener noreferrer">GitHub</a>
           </div>
         </div>
         <div className='Project-item'>
@@ -54,7 +59,7 @@ function Projects() {
           <img src='betterLife.png' alt='Project 6' />
           <div className='Project-links'>
             <a href='#' class='Link-tbc' data-tooltip='tbc'>Live</a>
-            <a href='https://github.com/Mohamed1756/BetterLife'>GitHub</a>
+            <a href='https://github.com/Mohamed1756/BetterLife' target="_blank" rel="noopener noreferrer">GitHub</a>
           </div>
         </div>
         </div>
